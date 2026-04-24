6864efacc9b5a01e1dd60923d437c8d313d9d7e5
boxtersh <boxtersh@yandex.ru>
2026-04-24 21:34:23 +0300

    add file databases

---
+++ databases.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = 'sqlite:///movies.db'
+engine = create_engine(DATABASE_URL, echo=True, future=True)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
